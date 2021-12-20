6d6ab14daeb203e2634eb02bfd4f12bd7cbe82f6
Seth-McKilla <seth@endevrs.dev>
2021-12-19 22:03:33 -0500

    Add search bar

--- pages/the-band.tsx
+++ pages/the-band.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import useSWR from "swr";
@@ -6,6 +7,7 @@ import fetcher from "../utils/fetcher";
 // Mui
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
 import LinearProgress from "@mui/material/LinearProgress";
 
 // Components
@@ -19,6 +21,25 @@ type Member = {
 
 const Manifesto: NextPage = () => {
   const { data } = useSWR("/api/get-users", fetcher);
+  const [members, setMembers] = useState([] as Member[]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    if (data) {
+      setMembers(data.users);
+    }
+  }, [data]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const search = e.target.value;
+
+    setSearch(search);
+    setMembers(
+      data.users.filter((user: Member) =>
+        user.username.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  };
 
   return (
     <div>
@@ -29,23 +50,38 @@ const Manifesto: NextPage = () => {
       </Head>
 
       <Layout>
-        <Grid container spacing={3}>
+        <Grid container spacing={3} direction="column" alignItems="center">
           <Grid item xs={12}>
-            <Typography variant="h2" align="center" gutterBottom>
+            <Typography variant="h2" align="center">
               The Band
             </Typography>
-            {!data ? (
-              <LinearProgress />
-            ) : (
-              <Grid container spacing={3}>
-                {data.users.map(({ id, username, avatar }: Member) => (
-                  <Grid key={id} item xs={12} sm={6} md={3}>
-                    <CardMember username={username} avatar={avatar} />
-                  </Grid>
-                ))}
-              </Grid>
-            )}
           </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              label="Search Members"
+              placeholder="Search by name, skills, etc."
+              sx={{
+                minWidth: "300px",
+                textAlign: "center",
+                [`& fieldset`]: {
+                  borderRadius: 5,
+                },
+              }}
+              value={search}
+              onChange={handleChange}
+            />
+          </Grid>
+
+          {!members ? (
+            <LinearProgress />
+          ) : (
+            members.map(({ id, username, avatar }: Member) => (
+              <Grid key={id} container item xs={12} sm={6} md={3}>
+                <CardMember username={username} avatar={avatar} />
+              </Grid>
+            ))
+          )}
         </Grid>
       </Layout>
     </div>
